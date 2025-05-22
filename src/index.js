364fed10e81165fa0162eb20738f92ccc66a8cd3
Alexander O'Neill <aloneill@gmail.com>
2025-05-22 11:50:53 -0300

    Update to Mirador v. 3.4.1.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Mirador from 'mirador/dist/es/src/index';
 import MiradorDownload from 'mirador-dl-plugin';
 import MiradorDownloadDialogPlugin from 'mirador-dl-plugin';
-import miradorAnnotationPlugins from 'mirador-annotations/es/index';
-import LocalStorageAdapter from 'mirador-annotations/es/LocalStorageAdapter';
+
+
 import { miradorImageToolsPlugin } from 'mirador-image-tools';
 import textOverlayPlugin from 'mirador-textoverlay/es';
 
@@ -13,6 +13,6 @@ window.miradorPlugins = [
   ...textOverlayPlugin,
   MiradorDownload.miradorDownloadPlugin,
   MiradorDownload.MiradorDownloadDialogPlugin,
-  ...miradorAnnotationPlugins,
+
 ];
 
